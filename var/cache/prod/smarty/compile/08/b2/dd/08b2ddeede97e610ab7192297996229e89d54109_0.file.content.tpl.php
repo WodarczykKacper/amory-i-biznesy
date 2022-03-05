ab1585b5afa978ef4558977b328cc899b1e53e47
kacper <kofficher@gmail.com>
2022-03-05 19:28:30 +0100

    new version

---
+++ var/cache/prod/smarty/compile/08/b2/dd/08b2ddeede97e610ab7192297996229e89d54109_0.file.content.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/* Smarty version 3.1.39, created on 2022-03-05 19:22:21
+  from 'C:\xampp\htdocs\grawstepna\admin801ea3qk6\themes\new-theme\template\content.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_6223aa5d602632_82714939',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '08b2ddeede97e610ab7192297996229e89d54109' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\grawstepna\\admin801ea3qk6\\themes\\new-theme\\template\\content.tpl',
+      1 => 1643096356,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6223aa5d602632_82714939 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div id="ajax_confirmation" class="alert alert-success" style="display: none;"></div>
+
+
+<?php if ((isset($_smarty_tpl->tpl_vars['content']->value))) {?>
+  <?php echo $_smarty_tpl->tpl_vars['content']->value;?>
+
+<?php }
+}
+}
